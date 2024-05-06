0314255f40bdbfc5b48f68f7bc968e39719419d3
Egin Pardewan <eginpardewan@gmail.com>
2024-05-06 10:51:14 +0700

    Add project files.

---
+++ friendclass/friendclass.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang 
+{
+private:
+	string nama;
+public:
+	void setNama(string pNama);
+	friend class siswa;
+};
+
+class siswa 
+{
+private:
+	int id;
+public:
+	void setId(int Pid);
+	void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+}
+
+void siswa::setId(int pId) {
+	id = pId;
+}
+
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
+int main() {
+	orang o;
+	o.setNama("Joko Kumat");
+	siswa s;
+	s.setId(1);
+	s.displayAll(o);
+	cout << endl;
+	return 0;
+}
